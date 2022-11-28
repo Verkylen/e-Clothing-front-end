15bd75684e78992b22ba53051a57806e878b9a09
vsisterolli <vsisterolli@gmail.com>
2022-11-28 17:30:30 -0300

    feat: delete products

--- src/CartPage.js
+++ src/CartPage.js
@@ -9,36 +9,6 @@ import boldMinus from "./images/boldMinus.svg";
 import boldPlus from "./images/boldPlus.svg";
 import remove from "./images/remove.svg";
 
-const testProducts = [
-    {
-        "name": "name",
-        "image": "https://cdn-images-1.medium.com/fit/t/1600/480/1*ijlOdXRPEfGxMxcgiGExlA.png",
-        "price": "212.19",
-        "rate": "4.5",
-        "category": "category",
-        "amount": "1",
-        "_id": "_id"
-    },
-    {
-        "name": "name",
-        "image": "https://cdn-images-1.medium.com/fit/t/1600/480/1*ijlOdXRPEfGxMxcgiGExlA.png",
-        "price": "212.19",
-        "rate": "4.5",
-        "category": "category",
-        "amount": "1",
-        "_id": "_id"
-    },
-    {
-        "name": "name",
-        "image": "https://cdn-images-1.medium.com/fit/t/1600/480/1*ijlOdXRPEfGxMxcgiGExlA.png",
-        "price": "212",
-        "rate": "4.5",
-        "category": "category",
-        "amount": "4",
-        "_id": "_id"
-    }
-]
-
 export default function CartPage({selectedProducts, setSelectedProducts, totalPrice, setTotalPrice}) {
     const navigate = useNavigate();
     const [user] = useContext(userContext);
@@ -69,11 +39,11 @@ export default function CartPage({selectedProducts, setSelectedProducts, totalPr
                         <span>R${Number(price).toFixed(2)}</span>
                     </div>
                     <div>
-                        <img onClick={() => deleteProduct(_id)} src={remove} alt=""/>
+                        <img onClick={() => deleteProduct(product, _id)} src={remove} alt=""/>
                         <div>
-                            <img onClick={() => handleAmount(-1, _id, amount)} src={boldMinus} alt="Adicionar uma unidade"/>
+                            <img onClick={() => handleAmount(-1, product, amount)} src={boldMinus} alt="Adicionar uma unidade"/>
                             <span>{amount}</span>
-                            <img onClick={() => handleAmount(1, _id, amount)} src={boldPlus} alt="Retirar uma unidade"/>
+                            <img onClick={() => handleAmount(1, product, amount)} src={boldPlus} alt="Retirar uma unidade"/>
                         </div>
                     </div>
                 </section>
@@ -81,26 +51,34 @@ export default function CartPage({selectedProducts, setSelectedProducts, totalPr
         );
     }
 
-    function handleAmount(quantity, _id, currentAmount) {
+    function handleAmount(quantity, product, currentAmount) {
         if (disabled === false) {
             if (Number(currentAmount) + quantity === 0) return;
 
             setDisabled(true);
+            const sentableProduct = {
+                "amount": quantity,
+                "color": product.color,
+                "size": product.size
+            };
 
             const config = {headers: {"Authorization": "Bearer " + user.sessionId}};
-
-            axios.post(API_BASE_URL + `/product/${_id}`, {amount: quantity}, config)
+            axios.post(API_BASE_URL + `/products/${product._id}`, sentableProduct, config)
                 .then(() => {setRefresh(!refresh); setDisabled(false)});
         }
     }
 
-    function deleteProduct(_id) {
+    function deleteProduct(product, _id) {
         if (disabled === false) {
             setDisabled(true);
-
-            const config = {headers: {"Authorization": "Bearer " + user.sessionId}};
+            const sentableProduct = {
+                "amount": 0,
+                "color": product.color,
+                "size": product.size
+            };
+            const config = {headers: {"Authorization": "Bearer " + user.sessionId}, data: sentableProduct};
     
-            axios.delete(API_BASE_URL + `/product/${_id}`, config)
+            axios.delete(API_BASE_URL + `/products/${_id}`, config)
                 .then(() => {setRefresh(!refresh); setDisabled(false)});
         }
     }
--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://eclothing-api.onrender.com";
-//const API_BASE_URL = "//localhost:5000"
+//const API_BASE_URL = "https://eclothing-api.onrender.com";
+const API_BASE_URL = "//localhost:5000"
 
 export default API_BASE_URL;
